f52bc44e29465f80b1b8a91ad7346dc89c30ad59
hakhis <b2mieszkowski@gmail.com>
2020-04-14 21:16:44 +0200

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+        //Test for SimpleUser
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        //Tests for Calculator
+        Calculator calculator = new Calculator();
+        //Adding
+        if(calculator.add(1, 14) == 15){
+            System.out.println("Test for add OK");
+        } else {
+            System.out.println("add Error!");
+        }
+        //Subtracting
+        if(calculator.subtract(10, 4) == 6){
+            System.out.println("Test for subtract OK");
+        } else {
+            System.out.println("subtract Error!");
+        }
     }
 }
