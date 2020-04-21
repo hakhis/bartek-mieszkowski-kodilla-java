b7b05fa672c4cc639ba03ee1b3d52afb1379540b
hakhis <b2mieszkowski@gmail.com>
2020-04-21 20:32:45 +0200

    Zadanie 7.1: Upiększacz tekstów

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,43 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
+        /* Wyrażenia lambda
         Processor processor = new Processor();
         Executor codeToExecute = () -> System.out.println("This is an example text.");
         processor.execute(codeToExecute);
+        processor.execute(() -> System.out.println("This is an example text 2."));
+        //*/
+
+        /* Wyrażenia lambda i referencje do metod
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        //*/
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String sampleText = "Szedł Sasza suchą szosą";
+        System.out.println(poemBeautifier.beautify(sampleText, txt -> "ABC " + sampleText + " ABC"));
+        System.out.println(poemBeautifier.beautify(sampleText, txt -> sampleText.toUpperCase()));
+        System.out.println(poemBeautifier.beautify(sampleText, txt -> sampleText.toLowerCase()));
+        System.out.println(poemBeautifier.beautify(sampleText, txt -> "( ͡° ͜ʖ ͡°) " + sampleText + " ( ͡° ͜ʖ ͡°)"));
+
     }
 }
