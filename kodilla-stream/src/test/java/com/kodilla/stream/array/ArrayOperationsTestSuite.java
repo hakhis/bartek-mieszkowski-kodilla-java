4e2bec483a64b29c7074c33720cb3fd5ad3e14f5
hakhis <b2mieszkowski@gmail.com>
2020-04-23 23:09:02 +0200

    do zadania 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] arrayOfNumbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arrayOfNumbers[i] = (i + 1) * 100;
+        }
+
+        //When
+
+        //Then
+    }
+}
