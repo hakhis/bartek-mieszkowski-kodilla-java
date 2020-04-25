34e09ef67246129cc63aab6fa9d3a7ded79968c0
hakhis <b2mieszkowski@gmail.com>
2020-04-25 21:11:38 +0200

    Zadanie 7.5 obliczamy średnią, funkcyjnie

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,5 +1,20 @@
 package com.kodilla.stream.array;
 
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
-    double getAverage(int[] numbers);
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average().getAsDouble();
+
+        //Na poczatku nie doczytałem i zrobiłem tak jak niżej i też działało :)
+        /*IntStream.of(numbers)
+                .forEach(System.out::println);
+        return IntStream.of(numbers)
+                .average().getAsDouble();*/
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,18 +1,28 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
+        int n = 10;
         int[] arrayOfNumbers = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < n; i++) {
             arrayOfNumbers[i] = (i + 1) * 100;
         }
+        //average calculation
+        double calculatedAverage = 0;
+        for (int i = 0; i < n; i++) {
+            calculatedAverage += arrayOfNumbers[i];
+        }
+        calculatedAverage = calculatedAverage / 10;
 
         //When
+        double streamAverage = ArrayOperations.getAverage(arrayOfNumbers);
 
         //Then
+        Assert.assertEquals(calculatedAverage, streamAverage, 0.001);
     }
 }
