bddfd3c4156ec5783cbe2379709e6a148fddaab0
hakhis <b2mieszkowski@gmail.com>
2020-06-02 23:20:50 +0200

    try

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreReader.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreReader.java
@@ -1,5 +1,19 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class MovieStoreReader {
 
+    public static void main(String[] args) {
+        Map<String, List<String>> map = MovieStore.getMovies();
+        List<String> movieList = map.entrySet().stream()
+                .flatMap(e -> e.getValue().stream()
+                .map(l -> ))
+
+                .collect(Collectors.toList());
+        movieList.stream().forEach(System.out::println);
+
+    }
 }
